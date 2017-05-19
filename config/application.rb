8716d475def727908928889eb8e6b6d1666ccb04
Andy Lindeman <andy@lindeman.io>
2017-05-19 14:24:09 -0400

    Adds simple config registry

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,23 @@ module ScanDonation
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.autoload_paths << Rails.root.join("lib")
+  end
+
+  def self.config
+    @config ||= Configuration.new
+  end
+
+  class Configuration
+    def salesforce_client
+      Salesforce::Client.new(
+        client_id:      ENV.fetch("SALESFORCE_CLIENT_ID"),
+        client_secret:  ENV.fetch("SALESFORCE_CLIENT_SECRET"),
+        username:       ENV.fetch("SALESFORCE_USERNAME"),
+        password:       ENV.fetch("SALESFORCE_PASSWORD"),
+        security_token: ENV.fetch("SALESFORCE_SECURITY_TOKEN")
+      )
+    end
   end
 end
