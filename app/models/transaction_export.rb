3086e8161dd09f2d4e2485afa72b99bcae06eec7
Chris Lamb <lamb.chrisr@gmail.com>
2017-05-20 15:13:28 -0400

    Persist transaction from square

--- app/models/transaction_export.rb
+++ app/models/transaction_export.rb
@@ -1,4 +1,6 @@
 class TransactionExport
+  ANONYMOUS_SALESFORCE_CUSTOMER_ID = '00331000031Tt8i'.freeze
+
   def export_to_salesforce(pagination_cursor: nil)
     square_client = ScanDonation.config.square_client
 
@@ -7,10 +9,11 @@ class TransactionExport
 
     Array(results&.transactions).each do |transaction|
       begin
-        synchronize_square_transaction(transaction)
+        create_square_transaction(transaction)
       rescue => e
         Rails.logger.error(e)
         Rollbar.error(e)
+        raise if Rails.env.test?
       end
     end
 
@@ -21,17 +24,33 @@ class TransactionExport
     end
   end
 
-  def synchronize_square_transaction(transaction)
+  def create_square_transaction(transaction)
+    return unless transaction.tenders&.count > 0
+
+    if transaction.tenders.count > 1
+      Rollbar.warning("Transaction(#{transaction.id}) has more then one tender.")
+    end
+    tender      = transaction.tenders.first
+    customer_id =
+      if tender.customer_id.present?
+        #TODO: What if we don't find the customer?
+        SquareCustomer.find_by!(square_id: tender.customer_id).salesforce_id
+      else
+        ANONYMOUS_SALESFORCE_CUSTOMER_ID
+      end
+
     square_transaction = SquareTransaction.find_or_initialize_by(square_id: transaction.id)
     if square_transaction.new_record?
-      salesforce_id = push_transaction_to_salesforce(transaction)
+      #salesforce_id = push_donation_to_salesforce(
+        #amount:      tender.amount_money,
+        #customer_id: customer_id,
+        #created_at:  tender.created_at,
+      #)
+      #TODO: Integrate with salesforce
+      salesforce_id = SecureRandom.hex
 
       square_transaction.salesforce_id = salesforce_id
       square_transaction.save!
     end
-  end
-
-  def begin_time
-    5.days.ago
   end
 end
--- test/models/transaction_export_test.rb
+++ test/models/transaction_export_test.rb
@@ -3,7 +3,8 @@ require 'minitest/mock'
 
 class TransactionExportTest < ActiveSupport::TestCase
   setup do
-    @response_body = { transactions: [{ id: "abc123", tenders: [], updated_at: 1.day.ago }] }.to_json
+    @tenders = [{ customer_id: "321cba", created_at: 5.hours.ago, amount_money: 4200}]
+    @response_body = { transactions: [{ id: "abc123", tenders: @tenders, updated_at: 1.day.ago }] }.to_json
 
     ENV["SALESFORCE_CLIENT_ID"] = 'client_id'
     ENV["SALESFORCE_CLIENT_SECRET"] = 'client_secret'
@@ -12,12 +13,16 @@ class TransactionExportTest < ActiveSupport::TestCase
     ENV["SALESFORCE_SECURITY_TOKEN"] = 'security_token'
     ENV["SQUARE_API_KEY"] = 'api_key'
 
+    SquareCustomer.create!(square_id: '321cba', salesforce_id: '42')
+
     stub_request(:get, "https://connect.squareup.com/v2/locations/FZS7GXYFJ6HRB/transactions?sort_order=ASC").
       with(headers: {'Accept'=>'application/json', 'Authorization'=>'Bearer api_key', 'Content-Type'=>'application/json', 'Expect'=>'', 'User-Agent'=>'Square-Connect-Ruby/2.0.2'}).
       to_return(status: 200, body: @response_body, headers: {})
   end
 
   test 'requests transactions from square' do
-    TransactionExport.new.export_to_salesforce
+    assert_difference('SquareTransaction.count', 1) do
+      TransactionExport.new.export_to_salesforce
+    end
   end
 end
